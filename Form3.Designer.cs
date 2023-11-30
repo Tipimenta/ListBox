7bffe9a82380475cc40dcfa19807837e122f4387
Tipimenta <tiagopimenta.ata@gmail.com>
2023-11-30 20:53:14 -0300

    primeiro commit

---
+++ Form3.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CestasDaPrima
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvItens = new System.Windows.Forms.ListView();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsvItens
+            // 
+            this.lsvItens.HideSelection = false;
+            this.lsvItens.Location = new System.Drawing.Point(13, 13);
+            this.lsvItens.Name = "lsvItens";
+            this.lsvItens.Size = new System.Drawing.Size(285, 425);
+            this.lsvItens.TabIndex = 0;
+            this.lsvItens.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(417, 362);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(141, 64);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar Produto";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lsvItens);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvItens;
+        private System.Windows.Forms.Button btnAdicionar;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CestasDaPrima
+{
+
+    public partial class Form3 : Form
+    {
+
+
+        List<Produto> lista_produtos = new List<Produto>
+        {
+            new Produto {Id = 1, Nome = "Café", Preco = 2.5m},
+            new Produto {Id = 2, Nome = "Pão", Preco = 4.0m},
+            new Produto {Id = 3, Nome = "Leite", Preco = 3.0m},
+            new Produto {Id = 4, Nome = "Cereal", Preco = 5.5m},
+            new Produto {Id = 5, Nome = "Frutas", Preco = 6.8m},
+            new Produto {Id = 6, Nome = "Iogurte", Preco = 3.2m},
+            new Produto {Id = 7, Nome = "Ovos", Preco = 7.5m},
+            new Produto {Id = 8, Nome = "Queijo", Preco = 5.0m},
+            new Produto {Id = 9, Nome = "Manteiga", Preco = 2.0m},
+            new Produto {Id = 10, Nome = "Geleia", Preco = 4.5m},
+            new Produto {Id = 11, Nome = "Croissant", Preco = 3.8m},
+            new Produto {Id = 12, Nome = "Suco de Laranja", Preco = 4.9m},
+            new Produto {Id = 13, Nome = "Panquecas", Preco = 6.2m},
+            new Produto {Id = 14, Nome = "Granola", Preco = 5.7m},
+            new Produto {Id = 15, Nome = "Café com Leite", Preco = 3.7m},
+            new Produto {Id = 16, Nome = "Waffles", Preco = 4.5m},
+            new Produto {Id = 17, Nome = "Torrada", Preco = 2.0m},
+            new Produto {Id = 18, Nome = "Presunto", Preco = 5.3m},
+            new Produto {Id = 19, Nome = "Cappuccino", Preco = 4.2m},
+            new Produto {Id = 20, Nome = "Smoothie", Preco = 6.5m},
+            new Produto {Id = 21, Nome = "Bolo de Café", Preco = 8.0m},
+            new Produto {Id = 22, Nome = "Cuscuz", Preco = 3.4m},
+            new Produto {Id = 23, Nome = "Muffin", Preco = 2.8m},
+            new Produto {Id = 24, Nome = "Aveia", Preco = 4.3m},
+            new Produto {Id = 25, Nome = "Sanduíche de Bacon e Ovo", Preco = 7.8m},
+            new Produto {Id = 26, Nome = "Chá", Preco = 2.3m},
+            new Produto {Id = 27, Nome = "Batata Doce Assada", Preco = 5.1m},
+            new Produto {Id = 28, Nome = "Mel", Preco = 3.5m},
+            new Produto {Id = 29, Nome = "Salada de Frutas", Preco = 6.0m},
+            new Produto {Id = 30, Nome = "Pão de Banana", Preco = 4.7m}
+        };
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            configuraListView();
+        }
+
+        void configuraListView() 
+        {
+            lsvItens.View = View.Details;
+            lsvItens.Columns.Add("ID");
+            lsvItens.Columns.Add("Nome");
+            lsvItens.Columns.Add("Preço");
+        }
+
+        void AdicionarItemListView(Produto produto)
+        {
+            ListViewItem item = new ListViewItem(produto.Id.ToString());
+            item.SubItems.Add(produto.Nome);
+            item.SubItems.Add(produto.Preco.ToString("c"));
+
+            lsvItens.Items.Add(item);          
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            Produto produto;
+
+            produto.Id = 1;
+            produto.Nome = "Chocolate";
+            produto.Preco = 19.9m;
+
+            AdicionarItemListView(produto);
+        }
+    }
+}
